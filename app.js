93fdfa0c8257598476d79a5ddae6e5ea218cd4e4
CN 8 <administrator@CN-8.lan>
2019-03-26 16:01:52 +0000

    css

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-function keycode(){
-    var key = event.keyCode; 
-    }
+const event1 = document.getElementById("demo")
+const event2 = document.getElementById("demo1")
+const event3 = document.getElementById("demo2")
+const event4 = document.getElementById("demo3")
+
+
+
+document.addEventListener("keypress", () => {
+   event1.innerHTML = event.which;
+   event2.innerHTML = " " + event.code;
+   event3.innerHTML = " " + event.which;
+   event4.innerHTML = " " + event.key;
+});
